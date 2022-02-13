37704f4c06f29944d51f19ee69fe76af42fb4556
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-02-13 10:38:46 +0700

    upload github

---
+++ app/dashboard/controller.js
@@ -0,0 +1,12 @@
+module.exports = {
+    index: async (req, res) => {
+        try {
+            res.render('index', { 
+                name: req.session.user.name,
+                title: 'Halaman Dashboard'
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
